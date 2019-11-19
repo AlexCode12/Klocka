f5ef5a6a313c57f35992a725902053ba9fcac5ce
alexcode12 <alexander.olsson2@elev.ga.ntig.se>
2019-11-19 09:18:22 +0100

    JavaDoc inkluderat

--- src/Clock.java
+++ src/Clock.java
@@ -3,32 +3,62 @@ private NumberDisplay hours;
 private NumberDisplay minutes;
 private String displayString;
 
+    /**
+     * Konstruktor som anropas varje gång klockan skapas
+     */
     public Clock () {
         this.hours = new NumberDisplay(24);
         this.minutes = new NumberDisplay(60);
         updateDisplay();
     }
 
+    /**
+     * Sätter värdena till 00:00
+     * @param hours räknar timmarna
+     * @param minutes räknar minuterna
+     */
     public Clock(int hours, int minutes) {
         this();
         this.hours.setValue(0);
         this.minutes.setValue(0);
     }
+
+    /**
+     * Denna metod adderar minuterna
+     */
     public void timeTick () {
         minutes.increment();
     } //Enbart struktur, måste aktiveras manuellt
 
+    /**
+     * Setter för tiden, se Clock för mer info
+     * @param hours
+     * @param minutes
+     */
     public void setTime(int hours, int minutes) {
         this.hours.setValue(hours);
         this.minutes.setValue(minutes);
     }
+
+    /**
+     * Getter för displayStringen
+     * @return
+     */
     public String getTime() {
         return displayString;
     }
+
+    /**
+     * Uppdaterar displayen.
+     */
     private void updateDisplay() {
     displayString = hours.getDisplayValue() + ":" + minutes.getDisplayValue();
     }
 
+    /**
+     * Testprogram för att se att allt funkar
+     * @param args
+     */
     public static void main(String[] args) {
         Clock c1 = new Clock();
         System.out.println(c1.getTime());
--- src/NumberDisplay.java
+++ src/NumberDisplay.java
@@ -2,14 +2,27 @@ public class NumberDisplay{
     protected int limit;
     protected int value;
 
-
+    /**
+     * Metod som sätter limiten.
+     * @param maxLimit
+     */
     public NumberDisplay(int maxLimit) {
         value = 0;
         limit = maxLimit;
     }
+
+    /**
+     * Getter för value
+     * @return
+     */
     public int getValue() {
         return value;
     }
+
+    /**
+     * Den här metoden är en failsafe för klockan. Om ett ogiltigt värde (ex -1) skrivs in händer inget.
+     * @param newValue
+     */
     public void setValue(int newValue) {
         if (newValue < 0 && newValue > limit) {
 
@@ -17,13 +30,22 @@ public class NumberDisplay{
             this.value = newValue;
         } //Om värderna skulle vara -1 och neråt, samt 60 och uppåt gör inte klockan något
     }
+
+    /**
+     * Returnerar displayen, med designen XX:XX
+     * @return
+     */
     public String getDisplayValue() {
         String display = Integer.toString(value);
         if (value < 10) {
             display = "0" + display;
         }
-        return display; //Enbart design, enkelt sätt att få fram den extra nollan (ex 09).
+        return display;
     }
+
+    /**
+     * Adderar minuter
+     */
     public void increment() {
         value++;
         if (value == limit) {
